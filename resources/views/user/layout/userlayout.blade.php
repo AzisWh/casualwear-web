62959c4c2938e60f23ba495c699a4c37eeefcf24
azisssss <aziswh7@gmail.com>
2025-06-14 19:31:13 +0700

    test layout

--- resources/views/user/layout/userlayout.blade.php
+++ resources/views/user/layout/userlayout.blade.php
@@ -14,10 +14,10 @@
   <meta name="description" content="Bootstrap 5 Fashion Store HTML CSS Template">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  {{-- <link rel="stylesheet" type="text/css" href="{{asset('kaira/css/vendor.css')}}"> --}}
-  <link rel="stylesheet" href="kaira/css/style.css">
+  <link rel="stylesheet" href="{{asset('kaira/css/vendor.css')}}">
+  {{-- <link rel="stylesheet" href="kaira/css/style.css"> --}}
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-  <link rel="stylesheet" type="text/css" href="{{asset('kaira/style.css')}}">
+  <link rel="stylesheet" href="{{asset('kaira/style.css')}}">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
