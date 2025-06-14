ea370fe206d868b67af202199a81b51f51f2952a
azisssss <aziswh7@gmail.com>
2025-06-14 17:45:15 +0700

    navbar cart

--- resources/views/auth/layout/auth.blade.php
+++ resources/views/auth/layout/auth.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.9/dist/sweetalert2.min.css" rel="stylesheet">
 </head>
-<body class="bg-secondary">
+<body style="background: #F1F1F0">
     @include('sweetalert::alert')
     <div class="container py-5 ">
         @yield('content')
