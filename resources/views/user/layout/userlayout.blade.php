a6fd6630153209df99a33f8e04015988a7e312e5
azisssss <aziswh7@gmail.com>
2025-06-24 18:56:59 +0700

    swal not defined fix at checkout blade

--- resources/views/user/layout/userlayout.blade.php
+++ resources/views/user/layout/userlayout.blade.php
@@ -17,6 +17,7 @@
   <link rel="stylesheet" href="{{asset('kaira/css/vendor.css')}}">
   {{-- <link rel="stylesheet" href="kaira/css/style.css"> --}}
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.9/dist/sweetalert2.min.css" rel="stylesheet">
   <link rel="stylesheet" href="{{asset('kaira/style.css')}}">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -153,6 +154,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
     crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.9/dist/sweetalert2.all.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
   <script src="{{asset('kaira/js/script.min.js')}}"></script>
   @yield('script')
